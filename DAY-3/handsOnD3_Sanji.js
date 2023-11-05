16b866087e0ff100ad9c912e7fe5ab11b69ea4f3
sanjiwaniDewi <dewisanjiw@gmail.com>
2023-11-05 14:29:06 +0900

    handsOn DAY-3

---
+++ DAY-3/handsOnD3_Sanji.js
@@ -0,0 +1,39 @@
+//array of Object
+const array = [
+    {
+        nama: "Zoro",
+        nik: 322,
+        jenis_kelamin: "laki=laki",
+        golongan_darah: "O",
+        tempat_asal: "Jakarta",
+        alamat_domisili: "Jayapura",
+        hobi: ["tidur", "Gim", "Minum Jus"],
+    },
+    {
+        nama: "Luffi",
+        nik: 246,
+        jenis_kelamin: "laki=laki",
+        golongan_darah: "B",
+        tempat_asal: "Bekasi",
+        alamat_domisili: "Makasar",
+        hobi: ["Makan", "Mancing", "Joging"],
+    },
+    {
+        nama: "Sanji",
+        nik: 311,
+        jenis_kelamin: "laki-laki",
+        golongan_darah: "A",
+        tempat_asal: "Depok",
+        alamat_domisili: "Yogyakarta",
+        hobi: ["Memasak", "baca Buku", "ngemil"],
+    },
+    {
+        nama: "Nami",
+        nik: 571,
+        jenis_kelamin: "Perempuan",
+        golongan_darah: "A",
+        tempat_asal: "Bandung",
+        alamat_domisili: "Jakarta",
+        hobi: ["Belanja", "Perawatan", "Tidur"],
+    },
+];
